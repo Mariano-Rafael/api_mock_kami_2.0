4885f1a7566844130d13c60efd621571eb14da26
Mariano-Rafael <rmariano38@gmail.com>
2024-11-05 01:08:24 -0300

    correção nomes dos campos da tabela

--- src/main/java/pgmais/ia/api_mock_kami_20/models/Debt.java
+++ src/main/java/pgmais/ia/api_mock_kami_20/models/Debt.java
@@ -1,12 +1,13 @@
 package pgmais.ia.api_mock_kami_20.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 
+@JsonInclude(JsonInclude.Include.ALWAYS)
 @Entity
-@Table(name = "client_debts")
+@Table(name = "debts")
 public class Debt {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int clientId;
     private String document;
@@ -26,6 +27,9 @@ public class Debt {
     private String maximumInstallmentDueDate;
 
     // construtor
+
+    public Debt() {
+    }
 
     public Debt(Long id, int clientId, String document, String clientName, String contract, double paymentValue,
                 double originalValue, boolean allowRenegociation, String product, String status, String dueDate,
@@ -50,148 +54,140 @@ public class Debt {
         this.maximumInstallmentDueDate = maximumInstallmentDueDate;
     }
 
-    public Debt() {
-
-    }
-
-    // getters e setters
-
     public Long getId() {
         return id;
-    }
-
-
-    public int getClientId() {
-        return clientId;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public int getClientId() {
+        return clientId;
+    }
+
     public void setClientId(int clientId) {
         this.clientId = clientId;
-    }
-
-    public void setDocument(String document) {
-        this.document = document;
-    }
-
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
-    }
-
-    public void setContract(String contract) {
-        this.contract = contract;
-    }
-
-    public void setPaymentValue(double paymentValue) {
-        this.paymentValue = paymentValue;
-    }
-
-    public void setOriginalValue(double originalValue) {
-        this.originalValue = originalValue;
-    }
-
-    public void setAllowRenegociation(boolean allowRenegociation) {
-        this.allowRenegociation = allowRenegociation;
-    }
-
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setDueDate(String dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public void setOverDueDays(int overDueDays) {
-        this.overDueDays = overDueDays;
-    }
-
-    public void setClientCode(String clientCode) {
-        this.clientCode = clientCode;
-    }
-
-    public void setMinimumInstallmentNumber(int minimumInstallmentNumber) {
-        this.minimumInstallmentNumber = minimumInstallmentNumber;
-    }
-
-    public void setMaximumInstallmentNumber(int maximumInstallmentNumber) {
-        this.maximumInstallmentNumber = maximumInstallmentNumber;
-    }
-
-    public void setMinimumInstallmentDueDate(String minimumInstallmentDueDate) {
-        this.minimumInstallmentDueDate = minimumInstallmentDueDate;
-    }
-
-    public void setMaximumInstallmentDueDate(String maximumInstallmentDueDate) {
-        this.maximumInstallmentDueDate = maximumInstallmentDueDate;
     }
 
     public String getDocument() {
         return document;
     }
 
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
     public String getClientName() {
         return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
     public String getContract() {
         return contract;
     }
 
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
+
     public double getPaymentValue() {
         return paymentValue;
+    }
+
+    public void setPaymentValue(double paymentValue) {
+        this.paymentValue = paymentValue;
     }
 
     public double getOriginalValue() {
         return originalValue;
     }
 
+    public void setOriginalValue(double originalValue) {
+        this.originalValue = originalValue;
+    }
+
     public boolean isAllowRenegociation() {
         return allowRenegociation;
+    }
+
+    public void setAllowRenegociation(boolean allowRenegociation) {
+        this.allowRenegociation = allowRenegociation;
     }
 
     public String getProduct() {
         return product;
     }
 
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
     public String getStatus() {
         return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getDueDate() {
         return dueDate;
     }
 
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
     public int getOverDueDays() {
         return overDueDays;
+    }
+
+    public void setOverDueDays(int overDueDays) {
+        this.overDueDays = overDueDays;
     }
 
     public String getClientCode() {
         return clientCode;
     }
 
+    public void setClientCode(String clientCode) {
+        this.clientCode = clientCode;
+    }
+
     public int getMinimumInstallmentNumber() {
         return minimumInstallmentNumber;
+    }
+
+    public void setMinimumInstallmentNumber(int minimumInstallmentNumber) {
+        this.minimumInstallmentNumber = minimumInstallmentNumber;
     }
 
     public int getMaximumInstallmentNumber() {
         return maximumInstallmentNumber;
     }
 
+    public void setMaximumInstallmentNumber(int maximumInstallmentNumber) {
+        this.maximumInstallmentNumber = maximumInstallmentNumber;
+    }
+
     public String getMinimumInstallmentDueDate() {
         return minimumInstallmentDueDate;
+    }
+
+    public void setMinimumInstallmentDueDate(String minimumInstallmentDueDate) {
+        this.minimumInstallmentDueDate = minimumInstallmentDueDate;
     }
 
     public String getMaximumInstallmentDueDate() {
         return maximumInstallmentDueDate;
     }
 
+    public void setMaximumInstallmentDueDate(String maximumInstallmentDueDate) {
+        this.maximumInstallmentDueDate = maximumInstallmentDueDate;
+    }
 }
 
